fc35f95b2bac20973e87dd0407c2384c292fef1e
Denis <kirugahdennis@gmail.com>
2026-06-06 23:23:37 +0300

    Initial toy tales commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,22 +5,8 @@ import ToyForm from "./ToyForm";
 import ToyContainer from "./ToyContainer";
 
 function App() {
-  const [showForm, setShowForm] = useState(false);
-
-  function handleClick() {
-    setShowForm((showForm) => !showForm);
-  }
-
-  return (
-    <>
-      <Header />
-      {showForm ? <ToyForm /> : null}
-      <div className="buttonContainer">
-        <button onClick={handleClick}>Add a Toy</button>
-      </div>
-      <ToyContainer />
+  <>
     </>
-  );
 }
 
 export default App;
